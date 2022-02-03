d493e65f070b4e2b20fbddf324ff545c8dbeae9b
Aleksander-true <al_seryapin@mail.ru>
2022-02-03 19:48:00 +0300

    feat: add advantages block , add header and footer

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,21 +2,21 @@ import { Routes, Route } from 'react-router-dom';
 
 import Textbook from './../Pages/Textbook/Textbook'
 import Statistics from './../Pages/Statistics/Statistics';
-import About from './../Pages/About/About';
+import Authorization from './../Pages/Authorization/Authorization';
 import SprintGame from './../Pages/SprintGame/SprintGame';
 import AudioGame from './../Pages/AudioGame/AudioGame';
 import Home from './../Pages/Home/Home';
 
 function Main() {
   return (
-      <main className='container main'>
+      <main className='main'>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
           <Route path="/textbook" element={<Textbook />} />
           <Route path="/statistics" element={<Statistics />} />
           <Route path="/sprintGame" element={<SprintGame />} />
           <Route path="/audioGame" element={<AudioGame />} />
+          <Route path="/authorization" element={<Authorization />} />
         </Routes>
       </main>
   );
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,22 +1,54 @@
 import React from 'react';
+import dictionary from './../../assets/svg/dictionary_icon.svg'
+import games from './../../assets/svg/games_icon.svg'
+import statistics from './../../assets/svg/statistics_icon.svg'
+import textbook from './../../assets/svg/textbook_icon.svg'
+import { Link } from 'react-router-dom';
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: "Home"};
-  }
+import './advantages.css';
+import './home.css';
 
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
-
-  render() {
-    return (
-        <h1>{this.state.text}</h1>
-    );
-  }
+function Home() {
+  return (
+    <div className='home'>
+<div className='advantages'>
+        <h1 className='advantages__title'>Учите английский с <span className="marked">RSLang</span></h1>
+        <h4 className='advantages__subtitle'>Используй быстрый и эффективный способ изучения языка.</h4>
+        <ul className='advantages__list'>
+          <li className='advantages__item'>3600 самых часто используемых английских слов, покрывающих 90% обычного разговора.</li>
+          <li className='advantages__item'>Отмечайте слова для активного изучения, аудио примеры и картинки помогут запомнить лучше.</li>
+          <li className='advantages__item'>Сделайте изучение слов более увлекательным с помощью мини-игр.</li>
+          <li className='advantages__item'>Статистика - Следите за своим прогрессом каждый день.</li>
+        </ul>
+        <div className='advantages__icons-wrapper'>
+            <figure className='advantages__icon'> 
+              <Link to="/textbook">
+                <img className='icon_img' src={dictionary} alt='Словарь'></img>
+                <figcaption className='icon_desc'>Словарь</figcaption>
+              </Link>
+            </figure>
+            <figure className='advantages__icon'> 
+              <Link to="/textbook">
+                <img className='icon_img' src={textbook} alt='Учебник'></img>
+                <figcaption className='icon_desc'>Учебник</figcaption>
+              </Link>
+            </figure>
+            <figure className='advantages__icon'> 
+              <Link to="/sprintGame">
+                <img className='icon_img' src={games} alt='Игры'></img>
+                <figcaption className='icon_desc'>Игры</figcaption>
+              </Link>
+            </figure>
+          <figure className='advantages__icon'> 
+            <Link to="/statistics">
+              <img className='icon_img' src={statistics} alt='Статистика'></img>
+              <figcaption className='icon_desc'>Статистика</figcaption>
+            </Link>
+          </figure>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
