d1cfd924d68696f2722f7f26506c639f1aedfdf2
elina <elina.nep@gmail.com>
2022-02-05 00:32:05 +0300

    feat: results page

--- src/Pages/SprintGame/SprintGame.js
+++ src/Pages/SprintGame/SprintGame.js
@@ -4,7 +4,6 @@ import { words } from './words';
 const shuffledWords = () => {
   let arr = words.slice()
    arr.sort(()=>Math.random()-0.5)
-console.log(arr);
    return arr
 }
 
@@ -17,45 +16,90 @@ const wrongWords = [];
 function SprintGame () {
   return (
     <div>
-    {SprintButtons()}
-    {SprintStructure(currentWords)}
+    <SprintButtons/>
+    <SprintStructure words={currentWords}/>
     </div>
   )
 }
 
-const SprintStructure = (words) => {
+const SprintStructure = (props) => {
   const [count, setCount] = useState(0);
   const [maxSeries, setMaxSeries] = useState(0)
   const [wordNum, setWordNum] = useState(0)
-  const translateAnswer = randomAnswer(words, wordNum)
+  const translateAnswer = randomAnswer(props.words, wordNum)
   let timeLeft = 30;
-  const realAnswer = (words[wordNum].wordTranslate === translateAnswer);
+  const realAnswer = (props.words[wordNum].wordTranslate === translateAnswer);
 
   useEffect(() => {
     const keyPress = (event) => {
         if (event.keyCode === 39) { //right
-          handleClick(realAnswer, true, count, setCount, maxSeries, setMaxSeries, words, wordNum, setWordNum);}
+          handleClick(realAnswer, true, count, setCount, maxSeries, setMaxSeries, props.words, wordNum, setWordNum);}
         else if (event.keyCode === 37) { //left
-          handleClick(realAnswer, false, count, setCount, maxSeries, setMaxSeries, words, wordNum, setWordNum);
+          handleClick(realAnswer, false, count, setCount, maxSeries, setMaxSeries, props.words, wordNum, setWordNum);
         }
       }
     window.addEventListener('keydown', keyPress);
     return () => {
         window.removeEventListener('keydown', keyPress);
     };
-}, [count, maxSeries, realAnswer, wordNum, words]);
+  }, [count, maxSeries, realAnswer, wordNum, props.words]);
 
+return (
+  <div>
+{wordNum < WORDS_MAX-1 && QuestionsPage(count, setCount, maxSeries, setMaxSeries, timeLeft, wordNum, setWordNum, translateAnswer, realAnswer)}
+{wordNum === WORDS_MAX-1 && ResultsPage()}
+</div>)
+  
+}
+
+const QuestionsPage = (count, setCount, maxSeries, setMaxSeries, timeLeft, wordNum, setWordNum, translateAnswer, realAnswer) => {
   return (
   <div>
-    {showTimer(timeLeft)}
     <h3>Текущий результат: {count}</h3>
     <p>Угадано подряд: {maxSeries}</p>
+    {showTimer(timeLeft)}
     <div>  
-      {showEnglishWord(words, wordNum)}
+      {showEnglishWord(currentWords, wordNum)}
       {showTranslate(translateAnswer)}
-      {ShowButtons(words, wordNum, setWordNum, count, setCount, maxSeries, setMaxSeries, realAnswer)}
+      {ShowButtons(currentWords, wordNum, setWordNum, count, setCount, maxSeries, setMaxSeries, realAnswer)}
     </div>
   </div>
+  )
+}
+
+const ResultsPage = () => {
+  const correctWordsElements = correctWords.map(word =>
+    <WordString englishWord = {word.word} russianWord = {word.wordTranslate} sound = {word.audio} id = {word.id}/>
+   )
+  
+  const wrongWordsElements = wrongWords.map(word =>
+    <WordString englishWord = {word.word} russianWord = {word.wordTranslate} sound = {word.audio} id = {word.id}/>
+   )
+  
+  return (
+    <div>
+      <ul>
+        <p>correct</p>
+      {correctWordsElements}
+      <p>wrong</p>
+      {wrongWordsElements}
+      </ul>
+    </div>
+  )
+}
+
+const WordString = (props) => {
+ 
+  const englishWord = props.englishWord;
+  const russianWord = props.russianWord;
+  const soundURL = props.sound;
+  const id = props.id
+  return (
+    <li key = {id}>
+    <a href = {soundURL}>{SOUND_ICON} </a> 
+    {englishWord}
+     - 
+    {russianWord} </li>
   )
 }
 
@@ -107,10 +151,11 @@ const handleClick = (userAnswer, realAnswer, count, setCount, maxSeries, setMaxS
     wrongWords.push(words[wordNum]);
   }
 
-  if (wordNum<20) {
-  setWordNum(wordNum + 1);} else {
+  // if (wordNum<WORDS_MAX-1) {
+    
+  setWordNum(wordNum + 1);
+  
 
-  }
 }
 
 
@@ -136,14 +181,16 @@ const SprintButtons = () => {
         <path d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z"/>
       </svg>
 
-      <svg xml ns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-volume-up" viewBox="0 0 16 16">
-        <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
-        <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
-        <path d="M10.025 8a4.486 4.486 0 0 1-1.318 3.182L8 10.475A3.489 3.489 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.486 4.486 0 0 1 10.025 8zM7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12V4zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11z"/>
-      </svg>
+      {SOUND_ICON}
     </div>
   )
 }
+
+const SOUND_ICON = <svg xml ns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-volume-up" viewBox="0 0 16 16">
+<path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
+<path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
+<path d="M10.025 8a4.486 4.486 0 0 1-1.318 3.182L8 10.475A3.489 3.489 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.486 4.486 0 0 1 10.025 8zM7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12V4zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11z"/>
+</svg>
 
 
 export default SprintGame;
