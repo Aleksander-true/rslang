14b9a5e6bc8a389febdbe54cbf0912f54562939e
elina <elina.nep@gmail.com>
2022-02-04 20:57:14 +0300

    feat: key control

--- src/Pages/SprintGame/SprintGame.js
+++ src/Pages/SprintGame/SprintGame.js
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React, { useState, useEffect }  from 'react';
 import { words } from './words';
 
 const shuffledWords = () => {
@@ -9,36 +9,119 @@ console.log(arr);
 }
 
 const currentWords = shuffledWords();
-
 const WORDS_MAX = 20;
+const correctWords = [];
+const wrongWords = [];
 
-// class SprintGame extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {text: "SprintGame"};
-//   }
-
-//   componentDidMount() {
-//   }
-
-//   componentWillUnmount() {
-//   }
-
-//   render() {
-//     return (
-//         <h1>{this.state.text}</h1>
-//     );
-//   }
-// }
 
 function SprintGame () {
-  
   return (
     <div>
     {SprintButtons()}
     {SprintStructure(currentWords)}
     </div>
   )
+}
+
+const SprintStructure = (words) => {
+  const [count, setCount] = useState(0);
+  const [maxSeries, setMaxSeries] = useState(0)
+  const [wordNum, setWordNum] = useState(0)
+  const translateAnswer = randomAnswer(words, wordNum)
+  let timeLeft = 30;
+  const realAnswer = (words[wordNum].wordTranslate === translateAnswer);
+
+  useEffect(() => {
+    const keyPress = (event) => {
+        if (event.keyCode === 39) { //right
+          handleClick(realAnswer, true, count, setCount, maxSeries, setMaxSeries, words, wordNum, setWordNum);}
+        else if (event.keyCode === 37) { //left
+          handleClick(realAnswer, false, count, setCount, maxSeries, setMaxSeries, words, wordNum, setWordNum);
+        }
+      }
+    window.addEventListener('keydown', keyPress);
+    return () => {
+        window.removeEventListener('keydown', keyPress);
+    };
+}, [count, maxSeries, realAnswer, wordNum, words]);
+
+  return (
+  <div>
+    {showTimer(timeLeft)}
+    <h3>Текущий результат: {count}</h3>
+    <p>Угадано подряд: {maxSeries}</p>
+    <div>  
+      {showEnglishWord(words, wordNum)}
+      {showTranslate(translateAnswer)}
+      {ShowButtons(words, wordNum, setWordNum, count, setCount, maxSeries, setMaxSeries, realAnswer)}
+    </div>
+  </div>
+  )
+}
+
+const showTimer = (timeLeft) => {
+return (
+  <div>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-alarm" viewBox="0 0 16 16">
+    <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"/>
+    <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z"/>
+  </svg>
+  <p>{timeLeft}</p>
+  </div>
+)
+}
+
+const showEnglishWord = (words, wordNum) => {
+return (
+<div>
+  {words[wordNum].word}
+</div>
+)}
+
+const showTranslate = (translateAnswer) => {
+  return (
+    <div>
+      {translateAnswer}
+    </div>
+    )
+}
+
+
+const ShowButtons = (words, wordNum, setWordNum, count, setCount, maxSeries, setMaxSeries, realAnswer) => {
+  return (
+    <div>
+      <button className= "btn btn-danger" onClick={() => handleClick(false, realAnswer, count, setCount, maxSeries, setMaxSeries, words, wordNum, setWordNum)}>Нет</button>
+      <button className="btn btn-success" onClick={() => handleClick(true, realAnswer, count, setCount, maxSeries, setMaxSeries, words, wordNum, setWordNum)}>Да</button>
+    </div>
+  )
+}
+
+const handleClick = (userAnswer, realAnswer, count, setCount, maxSeries, setMaxSeries, words, wordNum, setWordNum) => {
+    
+  if (realAnswer === userAnswer) {
+    setCount(count+1);
+    setMaxSeries(maxSeries+1);
+    correctWords.push(words[wordNum]);
+  } else {
+    setMaxSeries(maxSeries=0)
+    wrongWords.push(words[wordNum]);
+  }
+
+  if (wordNum<20) {
+  setWordNum(wordNum + 1);} else {
+
+  }
+}
+
+
+const randomAnswer = (words, wordNum) => {
+ if (Math.round(Math.random())) {
+  
+  return (words[wordNum].wordTranslate)
+ } else {
+   
+  return (words[Math.floor(Math.random()*WORDS_MAX)].wordTranslate)
+ }
 }
 
 const SprintButtons = () => {
@@ -60,97 +143,6 @@ const SprintButtons = () => {
       </svg>
     </div>
   )
-}
-
-const SprintStructure = (words) => {
-  const [count, setCount] = useState(0);
-  const [maxSeries, setMaxSeries] = useState(0)
-  const [wordNum, setWordNum] = useState(0)
-  const translateAnswer = randomAnswer(words, wordNum)
-  let timeLeft = 30;
-
-  return (
-  <div>
-    {showTimer(timeLeft)}
-    <h3>Текущий результат: {count}
-      </h3>
-      <p>Угадано подряд: {maxSeries}</p>
-    <div>
-      
-      {showEnglishWord(words, wordNum)}
-      {showTranslate(translateAnswer)}
-      {ShowButtons(words, wordNum, translateAnswer, setWordNum, count, setCount, maxSeries, setMaxSeries)}
-    </div>
-  </div>
-  )
-}
-
-const showTimer = (timeLeft) => {
-
-return (
-  <div>
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-alarm" viewBox="0 0 16 16">
-    <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"/>
-    <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z"/>
-  </svg>
-  <p>{timeLeft}</p>
-  </div>
-)
-}
-
-const showEnglishWord = (words, wordNum) => {
-return (
-<div>
-  {words[wordNum].word}
-  </div>
-)
-}
-
-const showTranslate = (translateAnswer) => {
-  
-  return (
-    <div>
-      {translateAnswer}
-      </div>
-    )
-  
-}
-
-const ShowButtons = (words, wordNum, translateAnswer, setWordNum, count, setCount, maxSeries, setMaxSeries) => {
-  
-  const userAnswer = (words[wordNum].wordTranslate === translateAnswer);
-  console.log(userAnswer);
-  
-  const handleClick = (realAnswer) => {
-    
-    
-
-    if (realAnswer === userAnswer) {
-      setCount(count+1);
-      setMaxSeries(maxSeries+1);
-    } else {
-      setMaxSeries(maxSeries=0)
-    }
-
-    setWordNum(wordNum + 1);
-  }
-
-  return (
-    <div>
-      <button className= "btn btn-danger" onClick={() => handleClick(false)}>Нет</button>
-      <button className="btn btn-success" onClick={() => handleClick(true)}>Да</button>
-    </div>
-  )
-}
-
-const randomAnswer = (words, wordNum) => {
- if (Math.round(Math.random())) {
-  
-  return (words[wordNum].wordTranslate)
- } else {
-   
-  return (words[Math.floor(Math.random()*WORDS_MAX)].wordTranslate)
- }
 }
 
 
