8b4350956dcd4f9947a837959bbe37828a2cbfdb
elina <elina.nep@gmail.com>
2022-02-15 21:27:23 +0300

    fix: new date format

--- src/Pages/SprintGame/Components/Restart.tsx
+++ src/Pages/SprintGame/Components/Restart.tsx
@@ -10,17 +10,8 @@ type ResultBtnPropsType = {
 
 const ResultBtn:React.FC<ResultBtnPropsType> = ({ level, page }) => {
 
-    // const correctWordsElements = correctWords.map(word =>
-    //     <WordString englishWord={word.word} russianWord={word.wordTranslate} sound={word.audio} key={word.id} type={"v"} />
-    // )
-
-    // const wrongWordsElements = wrongWords.map(word =>
-    //     <WordString englishWord={word.word} russianWord={word.wordTranslate} sound={word.audio} key={word.id} type={'x'} />
-    // )
-
     return (
         <Button title='Играть с этими словами ещё' onClick={() => SprintGame(level, page)} type='success' classType='sprint__btns'/>
-       
     )
 }
 
--- src/Pages/SprintGame/util.tsx
+++ src/Pages/SprintGame/util.tsx
@@ -27,5 +27,5 @@ export const randomAnswer = (words: WordFromCollection[]) => {
 
 export const formDate = (date: Date) => {
 
-    return (`${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`)
+    return (`${String(date.getDate()).padStart(2, '0')}.${String(date.getMonth() + 1).padStart(2, '0')}.${date.getFullYear()}`)
 }
